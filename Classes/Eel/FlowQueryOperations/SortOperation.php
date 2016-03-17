c47fddef35bafd5206b31fcd7d094956387ceb97
Dominique Feyer <dfeyer@ttree.ch>
2016-03-17 13:54:49 +0100

    TASK: Initial commit

---
+++ Classes/Eel/FlowQueryOperations/SortOperation.php
@@ -0,0 +1,91 @@
+<?php
+namespace Ttree\ContentRepositoryUtility\Eel\FlowQueryOperations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Ttree.Neos.Toolbox".    *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Eel\FlowQuery\FlowQuery;
+use TYPO3\Eel\FlowQuery\FlowQueryException;
+use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\Node;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+
+/**
+ * EEL operation to sort nodes
+ */
+class SortOperation extends AbstractOperation
+{
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var string
+     */
+    static protected $shortName = 'sort';
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var integer
+     */
+    static protected $priority = 100;
+
+    /**
+     * {@inheritdoc}
+     *
+     * We can only handle TYPO3CR Nodes.
+     *
+     * @param mixed $context
+     * @return boolean
+     */
+    public function canEvaluate($context)
+    {
+        return (isset($context[0]) && ($context[0] instanceof NodeInterface)) || (is_array($context) && count($context) === 0);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param FlowQuery $flowQuery the FlowQuery object
+     * @param array $arguments the arguments for this operation
+     * @return mixed
+     */
+    public function evaluate(FlowQuery $flowQuery, array $arguments)
+    {
+        if (!isset($arguments[0]) || empty($arguments[0])) {
+            throw new FlowQueryException('sort() needs property name by which nodes should be sorted', 1332492263);
+        } else {
+            $nodes = $flowQuery->getContext();
+            $sortByPropertyPath = $arguments[0];
+            $sortOrder = 'DESC';
+            if (isset($arguments[1]) && !empty($arguments[1]) && in_array($arguments[1], array('ASC', 'DESC'))) {
+                $sortOrder = $arguments[1];
+            }
+
+            $sortedNodes = array();
+            $sortSequence = array();
+            $nodesByIdentifier = array();
+            /** @var Node $node */
+            foreach ($nodes as $node) {
+                $propertyValue = $node->getProperty($sortByPropertyPath);
+                if ($propertyValue instanceof \DateTime) {
+                    $propertyValue = $propertyValue->getTimestamp();
+                }
+                $sortSequence[$node->getIdentifier()] = $propertyValue;
+                $nodesByIdentifier[$node->getIdentifier()] = $node;
+            }
+            if ($sortOrder === 'DESC') {
+                arsort($sortSequence);
+            } else {
+                asort($sortSequence);
+            }
+            foreach ($sortSequence as $nodeIdentifier => $value) {
+                $sortedNodes[] = $nodesByIdentifier[$nodeIdentifier];
+            }
+            $flowQuery->setContext($sortedNodes);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+[![BSD License](https://img.shields.io/github/license/mashape/apistatus.svg)](LICENSE)
+[![Latest Stable Version](https://poser.pugx.org/ttree/contentrepositoryutility/version)](https://packagist.org/packages/ttree/contentrepositoryutility)
+
+# Neos Content Repository Utility
+
+This package contains utilities to work with the Neos Project Content Repository
+
+## FlowQuery Operations
+
+ * [FilterByDateOperation](Classes/Eel/FlowQueryOperations/FilterByDateOperation.php): Filter nodes by date properties
+ * [SortOperation](Classes/Eel/FlowQueryOperations/SortOperation.php): Sort nodes
+
+## Acknowledgments
+
+Development sponsored by [ttree ltd - neos solution provider](http://ttree.ch).
+
+We try our best to craft this package with a lots of love, we are open to sponsoring, support request, ... just contact us.
+
+## License
+
+The MIT License (MIT). Please see [LICENSE](LICENSE) for more information.
